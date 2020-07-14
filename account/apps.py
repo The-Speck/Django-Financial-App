6d44bf2983df0a93c6a26d2781af39672c0fa747
Alex <a.plyushchay@gmail.com>
2020-07-13 20:43:21 -0400

    start news app

--- account/apps.py
+++ account/apps.py
@@ -1,5 +1,5 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class AccountConfig(AppConfig):
-#     name = 'account'
+class AccountConfig(AppConfig):
+    name = 'account'
--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class News(APIView):
+    def get(self, request):
+        with open('..sample_data/news_api.json') as f:
+            response = json.load(f)
+
+        return Response(data=response, status=status.HTTP_200_OK)
